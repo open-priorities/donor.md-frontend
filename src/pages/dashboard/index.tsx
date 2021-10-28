31d625695c06dd289b46b96de98c3c07aee87bf4
Maxim Lambov <maxilamb@yandex.com>
2021-10-29 01:59:48 +0300

    fix margin mismatch fix match dashboard page

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,12 +1,27 @@
 import { useAuth } from '@Hooks/useAuth';
-import { useRequireAuth } from '@Hooks/useRequireAuth';
 import { HeaderContentFooter } from '@Layouts/header-content-footer';
+import { storage } from '@Services/storage';
+import { userAtom } from '@Store/atoms/user-atom';
 import { Loading } from '@UI/loading';
+import { useRouter } from 'next/dist/client/router';
+import { useEffect } from 'react';
+import { useRecoilValue } from 'recoil';
 import styled from 'styled-components';
 
 const Dashboard = () => {
   useAuth();
-  useRequireAuth();
+
+  const { push } = useRouter();
+  const user = useRecoilValue(userAtom);
+
+  useEffect(() => {
+    const isTokenExist = storage.get('token');
+    if (!user && !isTokenExist) {
+      push('/auth');
+    } else {
+      push('/dashboard/details');
+    }
+  }, [push, user]);
 
   return (
     <HeaderContentFooter>
